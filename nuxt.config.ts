962aaeacbd9a76a5d547fb3983828dd6416592b2
John Uberbacher <johnuberbacher@gmail.com>
2022-11-16 12:58:40 -0700

    set SSR mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,7 @@ export default defineNuxtConfig({
     css: [
         '@/assets/main.scss',
     ],
+    target: 'static',
+    mode: 'universal',
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt',]
 })
